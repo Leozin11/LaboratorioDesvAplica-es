cc97a240f4f342037ae4e8f32a1403b9d195a251
Leonardo Augusto Pereira do Carmo <78862366+Leozin11@users.noreply.github.com>
2025-06-18 17:45:27 -0300

    Add files via upload
    Criação da parte 2

---
+++ Lib/logitrack-backend/rastreamento-service/src/main/java/com/logitrack/rastreamentoservice/RastreamentoServiceApplication.java
@@ -0,0 +1,11 @@
+package com.logitrack.rastreamentoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreamentoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RastreamentoServiceApplication.class, args);
+    }
+}
